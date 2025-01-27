15596399098a81749022cbc0978fe47a00245bd8
JTR254 <jackrindge@gmail.com>
2025-01-26 20:49:38 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -9,5 +9,13 @@ products.push ("Pepper Grinder");
 
 console.log(products); // Output: [Salt Shaker, Pepper Shaker, Pepper Grinder]
 
+// Task 2
 
+let scores = [88, 77, 95, 65, 70]
+scores[1] = 82
+console.log(scores); //Output: [88, 82, 95, 65, 70]
 
+let totalScore = totalScore.reduce((total, amount) => total + amount, 0); // Sum of the scores = 400
+let averageScore = totalScore / scores.length // Output: 400/5 = 80
+
+console.log ("Average", averageScore)
