6c1d3c3bf6df0cd1f8bb619a02d4cf8a11d3f4fe
JTR254 <jackrindge@gmail.com>
2025-01-26 20:19:34 -0500

    Task 1 - Store Inventory

--- cc_2.js
+++ cc_2.js
@@ -1,1 +1,13 @@
-let products = ["Salt Shaker","Pepper Shaker", "Salt Block"]
+// Task 1
+
+let products = ["Salt Shaker","Pepper Shaker", "Salt Block"];
+let removed = products.pop();
+
+console.log(removed); // Output: ["Salt Block"]
+
+products.push ("Pepper Grinder");
+
+console.log(products); // Output: [Salt Shaker, Pepper Shaker, Pepper Grinder]
+
+
+
