e689be53276e2d543ade3f821f6c7e2154bd7191
JTR254 <jackrindge@gmail.com>
2025-01-26 20:54:21 -0500

    Task 2 - Student Scores (edited error)

--- cc_2.js
+++ cc_2.js
@@ -15,7 +15,7 @@ let scores = [88, 77, 95, 65, 70]
 scores[1] = 82
 console.log(scores); //Output: [88, 82, 95, 65, 70]
 
-let totalScore = totalScore.reduce((total, amount) => total + amount, 0); // Sum of the scores = 400
+let totalScore = Scores.reduce((total, amount) => total + amount, 0); // Sum of the scores = 400
 let averageScore = totalScore / scores.length // Output: 400/5 = 80
 
 console.log (totalScore)
